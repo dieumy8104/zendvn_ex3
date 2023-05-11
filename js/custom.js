3e01d1e09ee18e839c20037cc9cb507a425a4a6a
bao nguyen <mynguyen.vo0974@gmai.coml>
2023-05-11 21:48:56 +0700

    section exercise4

--- js/custom.js
+++ js/custom.js
@@ -1,69 +1,71 @@
 // exercise4
-let slideImages = document.querySelectorAll('img')
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
-let dots=document.querySelectorAll('.dot')
+const slideImages = document.getElementsByClassName('carousel-opacity')
+const slides = document.querySelector('.slide')
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const dots=document.querySelectorAll('.dot')
 // 
-var counter = 0;
+let index = 1;
+var counter=0
+var width;
+// 
+function slideWidth(){
+    width = slideImages[0].clientWidth;
+}
+slideWidth()
+window.addEventListener('resize', slideWidth)
+slides.style.transform = 'translateX('+(- width * index)+'px)';
 // 
 next.addEventListener('click', slideNext)
 function slideNext(){
-    slideImages[counter].style.animation='next1 0.65s ease-in forwards';
-    if(counter >= slideImages.length-1){
-        counter = 0 ;
+    if(index >= slideImages.length - 1){return};
+     slides.style.transition='transform 0.4s ease-out';
+        index++;
+        slides.style.transform='translateX(' +(- width * index) + 'px)';
+        dotsLabel()
     }
-    else{
-        counter++;
-    }
-    slideImages[counter].style.animation='next2 0.656s ease-in forwards';
-    indicators();
-}
 // 
 prev.addEventListener('click', slidePrev)
 function slidePrev(){
-    slideImages[counter].style.animation='prev1 0.65s ease-in forwards';
-    if(counter == 0){
-        counter = slideImages.length-1;
-    }
-    else{
-        counter--;
-    }
-    slideImages[counter].style.animation='prev2 0.65s ease-in forwrads';
-    indicators();
+    if(index <= 0){return};
+        slides.style.transition='transform 0.4s ease-out';
+        index-- ;
+        slides.style.transform='translateX(' +(- width * index) + 'px)';
+        dotsLabel()
 }
 // 
+slides.addEventListener('transitionend', function(){
+    if(slideImages[index].id === 'first'){
+    slides.style.transition = 'none'
+    index=slideImages.length-index;
+    slides.style.transform = 'translateX(' +(- width * index) + 'px)';
+    dotsLabel()
+    }
+    if(slideImages[index].id === 'last'){
+        slides.style.transition = 'none'
+        index=slideImages.length - 2;
+        slides.style.transform = 'translateX(' +(- width * index) + 'px)';
+        dotsLabel()
+    }
+})
+// 
 function autoSliding(){
-    deletInternal=setInterval(timer, 3000);
+    deleteInterval = setInterval(timer, 2500)
     function timer(){
-        slideNext();
-        indicators();
+        slideNext()
     }
 }
 autoSliding();
 // 
-function indicators(){
-    for(i = 0;i < dots.length; i++){
-        dots[i].className=dots[i].className.replace(' active', '');
+const container = document.querySelector('.slide-container')
+container.addEventListener('mouseover', function(){
+    clearInterval(deleteInterval)
+})
+container.addEventListener('mouseout',autoSliding)
+function dotsLabel(){
+    for(i = 0; i < dots.length; i++){
+        dots[i].className=dots[i].className.replace(' active', '')
     }
-    dots[counter].className +=' active'
-}
-// 
-function switchImage(currentImage){
-    currentImage.classList.add('active');
-    var imageId=currentImage.getAttribute('attr');
-    if(imageId>counter){
-        slideImages[counter].style.animation='next1 0.5s ease-in forwards';
-        counter=imageId;
-        slideImages[counter].style.animation='next2 0.5s ease-in forwards';
-    }
-    else if(imageId==counter){
-        return;
-    }
-    else{
-        slideImages[counter].style.animation='prev1 0.5s ease-in forwards';
-        counter=imageId;
-        slideImages[counter].style.animation='prev2 0.5s ease-in forwards';
-    }
-    indicators()
+    dots[index - 1].className += ' active'
 }
 // end exercise4
